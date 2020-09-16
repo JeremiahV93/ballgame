938eb780a9b0bc82c92a1b1433e5d2edc32ad3a3
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-09-15 22:09:58 -0500

    state is full updated and ready to create objects to submit to FB, need to split seat string when submitting

--- src/components/pages/GameForm/EventForm.js
+++ src/components/pages/GameForm/EventForm.js
@@ -35,12 +35,18 @@ class EventForm extends React.Component {
     this.setState({ awayTeamAcro: awayTeam.acronym, awayTeam: e.target.value });
   }
 
+  sectionEventChange = (e) => {
+    e.preventDefault();
+    this.setState({ section: e.target.value });
+  }
+
+  seatsEventChange = (e) => {
+    e.preventDefault();
+    this.setState({ seats: e.target.value });
+  }
+
   render() {
     const { stadiums, stadiumId, awayTeam } = this.state;
-
-    // const buildStadiumOptions = () => {
-    //   stadiums.forEach((stadium) => <option>{stadium.name}</option>);
-    // };
 
     // string split on a comma and turn it into an array
     return (
@@ -60,12 +66,14 @@ class EventForm extends React.Component {
 
         <div className="form-group">
           <label for="section">Section</label>
-          <input type="text" className="form-control" id="section" placeholder="Section" />
+          <input type="text" onChange={this.sectionEventChange} className="form-control" id="section" placeholder="Section" />
         </div>
+
         <div className="form-group">
           <label for="seats">Seats</label>
-          <input type="text" className="form-control" id="seats" placeholder="Please use commas between seats example: '1B, 2B, 3B'" />
+          <input type="text" onChange={this.seatsEventChange} className="form-control" id="seats" placeholder="Please use commas between seats example: '1B, 2B, 3B'" />
         </div>
+
         <button type="submit" className="btn btn-primary">Submit</button>
       </form>
     );
