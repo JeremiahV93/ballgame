fcf7f3e3c785e7acbb95fd2e34d83127dcbde3e4
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-09-15 19:01:01 -0500

    Data for Tickets and Events are pulled, now to create the page

--- src/components/pages/SingleEvent/SingleEvent.js
+++ src/components/pages/SingleEvent/SingleEvent.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import eventShape from '../../../helpers/props/eventShape';
 import eventData from '../../../helpers/data/eventData';
+import ticketData from '../../../helpers/data/ticketData';
 
 class SingleEvent extends React.Component {
   static propTypes = {
@@ -9,23 +10,29 @@ class SingleEvent extends React.Component {
 
   state = {
     event: {},
-    tickets: {},
+    ticket: {},
   }
 
   getEventData = () => {
     const { eventId } = this.props.match.params;
-    console.error(eventId);
     eventData.getEventById(eventId)
       .then((res) => this.setState({ event: res.data }))
       .catch((err) => console.error(err));
   }
 
   getTicketData = () => {
-
+    const { eventId } = this.props.match.params;
+    console.error(eventId);
+    ticketData.getTicketByEventId(eventId)
+      .then((res) => this.setState({ ticket: res }))
+      .catch((err) => console.error(err));
   };
+
+  // this.setState({ ticket: res[0] })
 
   componentDidMount() {
     this.getEventData();
+    this.getTicketData();
   }
 
   render() {
---
+++ src/helpers/data/ticketData.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+import utils from '../utils';
+
+const baseUrl = apiKeys.firebaseConfig.databaseURL;
+
+const getTicketByEventId = (id) => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/tickets.json?orderBy="eventId"&equalTo="${id}"`)
+    .then(({ data }) => {
+      const thing = utils.firebaseArray(data);
+      // if tickets being undefinded are an issue you can add an if statement
+      // here to resolve an empty object
+      resolve(thing[0]);
+    })
+    .catch((err) => reject(err));
+});
+
+export default { getTicketByEventId };
