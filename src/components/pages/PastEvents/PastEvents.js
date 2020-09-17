85ec9fb5be7bb41b37e5c801b8ff562fa4bb221b
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2020-09-17 10:12:14 -0500

    Past events page now loads like the home Screen

--- src/components/pages/PastEvents/PastEvents.js
+++ src/components/pages/PastEvents/PastEvents.js
@@ -1,10 +1,53 @@
 import React from 'react';
+import moment from 'moment';
+import authData from '../../../helpers/data/authData';
+import eventData from '../../../helpers/data/eventData';
+
+import VitalCards from '../VitalCards/VitalCards';
+
+import '../Home/Home.scss';
 
 class PastEvents extends React.Component {
+  state = {
+    pastEvents: [],
+    today: new Date(),
+  }
+
+  getEventData = () => {
+    eventData.getEventsByUid(authData.getUid())
+      .then()
+      .catch((err) => console.error(err));
+  }
+
+  getPastEventData = () => {
+    eventData.getEventsByUid(authData.getUid())
+      .then((data) => {
+        const { today } = this.state;
+        const pastEvents = [];
+        data.forEach((eachEvent) => {
+          if (moment(eachEvent.date).isBefore(today)) {
+            pastEvents.push(eachEvent);
+          }
+        });
+        this.setState({ pastEvents });
+      })
+      .catch((err) => console.error(err));
+  }
+
+  componentDidMount() {
+    this.getPastEventData();
+  }
+
   render() {
+    const { pastEvents } = this.state;
+
+    const vitalCards = pastEvents.map((event) => <VitalCards event={event} key={event.id} />);
+
     return (
       <div>
-        Past events
+        <div className='cards'>
+        { vitalCards }
+        </div>
       </div>
     );
   }
